2ecc29ff085d401848e36755a4a53293035a1289
zeptocosm <87965080+zeptocosm@users.noreply.github.com>
2022-03-21 00:39:46 +0000

    Update create_board.js

--- src/api/create_board.js
+++ src/api/create_board.js
@@ -8,17 +8,15 @@ const mysql = require("serverless-mysql")({
 		ssl : { ca : fs.readFileSync(process.env.CA_PATH || "/etc/pki/tls/certs/ca-bundle.crt") }
 	}
 });
-
+const sjcl = require("sjcl");
 const MS_PER_DAY = 1000 * 60 * 60 * 24;
-const ALPHABET = "abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789";
 
-const insecureRandomString = function(length) {
-	let string = "";
-	for (let i=0; i<length; i++) {
-		let index = Math.floor(Math.random() * ALPHABET.length); 
-		string += ALPHABET[index];
-	}
-	return string;
+// https://altv.stuyk.com/docs/articles/snippets/password-hashing.html
+const hashPassword = function(password) {
+	const saltBits = sjcl.random.randomWords(2, 0);
+	const salt = sjcl.codec.base64.fromBits(saltBits);
+	const key = sjcl.codec.base64.fromBits(sjcl.misc.pbkdf2(password, saltBits, 2000, 256));
+	return `${key}$${salt}`;
 };
 
 module.exports = async function(req, res) {
@@ -26,6 +24,7 @@ module.exports = async function(req, res) {
 	let lifespanDays = parseInt(body.lifespan_days);
 	if (isNaN(lifespanDays) || lifespanDays < 0 || lifespanDays > 35) {
 		res.status(400).send("Invalid lifespan_days");
+		mysql.end();
 		return;
 	}
 	// The next GMT midnight after now+lifespan:
@@ -38,31 +37,45 @@ module.exports = async function(req, res) {
 	let titleCt = body.title_ct;
 	if (!titleCt) {
 		res.status(400).send("Missing title_ct");
+		mysql.end();
 		return;
 	}
 	let descriptionCt = body.description_ct;
 	if (!descriptionCt) {
 		res.status(400).send("Missing description_ct");
+		mysql.end();
+		return;
+	}
+	let writingKey = body.writing_key;
+	if (!writingKey) {
+		res.status(400).send("Missing writing_key");
+		return;
+	}
+	let boardKey = body.board_key;
+	if (!boardKey) {
+		res.status(400).send("Missing board_key");
+		mysql.end();
 		return;
 	}
 
-	let boardCode;
-	while (true) {
-		boardCode = insecureRandomString(3);
-		let rows = await mysql.query(
-			"SELECT * FROM boards WHERE board_code=?",
-			[boardCode]
-		);
-		if (!rows.length) {
-			break;
-		}
+	let rows = await mysql.query(
+		"SELECT * FROM boards WHERE board_code=?",
+		[boardKey]
+	);
+	if (rows.length) {
+		// This is highly unlikely to happen by chance
+		res.status(400).send("board_key already exists");
+		mysql.end();
+		return;
 	}
+	let writingKeyHash = hashPassword(writingKey);
+
 	// There's a race condition here but it's very unlikely to happen
 	let result = await mysql.query(
-		`INSERT INTO boards (board_code, expiration_date_ms, title_ct, description_ct)
-		 VALUES (?,?,?,?)`, [boardCode,  expirationDateMs,   titleCt,  descriptionCt]);
+		`INSERT INTO boards (board_code, expiration_date_ms, title_ct, description_ct, writing_key_hash)
+		 VALUES (?,?,?,?,?)`, [boardKey, expirationDateMs,   titleCt,  descriptionCt,  writingKeyHash]);
 	console.log("INSERT INTO boards:", result);
 	
 	mysql.end()
-	res.send(boardCode);
+	res.status(200).send("OK");
 };

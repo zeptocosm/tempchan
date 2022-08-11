7d2f2364b484815102377215d498e248451d5d9b
zeptocosm <87965080+zeptocosm@users.noreply.github.com>
2022-08-11 02:42:58 +0000

    Update get_thread.js

--- src/api/get_thread.js
+++ src/api/get_thread.js
@@ -8,6 +8,20 @@ const mysql = require("serverless-mysql")({
 		ssl : { ca : fs.readFileSync(process.env.CA_PATH || "/etc/pki/tls/certs/ca-bundle.crt") }
 	}
 });
+const sjcl = require("sjcl");
+
+// https://altv.stuyk.com/docs/articles/snippets/password-hashing.html
+const verifyPassword = function(password, storedPasswordHash) {
+    const [_key, _salt] = storedPasswordHash.split('$');
+    const saltBits = sjcl.codec.base64.toBits(_salt);
+    const derivedKey = sjcl.misc.pbkdf2(password, saltBits, 2000, 256);
+    const derivedBaseKey = sjcl.codec.base64.fromBits(derivedKey);
+
+    if (_key != derivedBaseKey) {
+        return false;
+    }
+    return true;
+};
 
 module.exports = async function(req, res) {
 	let boardCode = req.query.board_code;
@@ -24,7 +38,7 @@ module.exports = async function(req, res) {
 	}
 	
 	let posts = await mysql.query(
-		`SELECT board_code, post_id, timestamp_ms, text_ct, parent_post_id, writers_only FROM posts
+		`SELECT board_code, post_id, timestamp_ms, text_ct, parent_post_id, writers_only, mod_status, author FROM posts
 		 WHERE board_code=? AND (post_id=? OR parent_post_id=?)
 		 ORDER BY timestamp_ms ASC`,
 		[boardCode, opId, opId]
@@ -34,7 +48,8 @@ module.exports = async function(req, res) {
 
 	let board = boards[0];
 	delete board.writing_key_hash;
-	delete board.owner_key;
+	board.moderated = !!board.owner_key_hash;
+	delete board.owner_key_hash;
 
 	res.send(JSON.stringify({
 		board: board,

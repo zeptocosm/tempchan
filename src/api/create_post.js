2107b584ceb4410248a16790690fb38aa4a7e8bb
zeptocosm <87965080+zeptocosm@users.noreply.github.com>
2022-08-19 22:37:51 +0000

    Update create_post.js

--- src/api/create_post.js
+++ src/api/create_post.js
@@ -79,28 +79,17 @@ module.exports = async function(req, res) {
 
 	// post_id should equal 1 plus the number of already-existing rows with the same
 	// board_code value.
-	let insertPostResult = await mysql.query(
-		`INSERT INTO posts (board_code, text_ct, parent_post_id, timestamp_ms, post_id, writers_only,      author)
-		 VALUES (?,?,?,?,?,?,?)`,
-		 [                  boardCode,  textCt,  parentPostId,   now,          0,       body.writers_only, author?1:0]);
+	let insertPostResult = await mysql.transaction()
+		.query(`SET @X = (SELECT COUNT(1) FROM posts WHERE board_code=?);`, [boardCode])
+		.query(`INSERT INTO posts (board_code, text_ct, parent_post_id, timestamp_ms, post_id, writers_only,      author)
+			 VALUES (?,?,?,?,@X+1,?,?);`,
+			                 [ boardCode,  textCt,  parentPostId,   now,                   body.writers_only, author?1:0])
+		.commit();
 	console.log("INSERT INTO posts:", insertPostResult);
-	let insertedIncrementingId = insertPostResult.insertId;
+	let insertedId = insertPostResult[1].insertId;
+	let getInsertedPostResult = await mysql.query(`SELECT post_id FROM posts WHERE incrementing_id=?`, [insertedId]);
 
-	let postsCountResult = await mysql.query(
-		"SELECT COUNT(1) FROM posts WHERE board_code=? AND incrementing_id<?",
-		[boardCode, insertedIncrementingId]
-	);
-	console.log("SELECT COUNT(1) FROM posts:", postsCountResult);
-
-	let postIdToSet = 1+postsCountResult[0]["COUNT(1)"];
-
-	let updatePostResult = await mysql.query(
-		"UPDATE posts SET post_id=? WHERE incrementing_id=?",
-		[postIdToSet, insertedIncrementingId]
-	);
-
-	console.log("UPDATE posts:", updatePostResult);
-	
 	mysql.end();
-	res.send(postIdToSet);
+	console.log("SELECT FROM posts:", getInsertedPostResult);
+	res.send(getInsertedPostResult[0].post_id);
 };
